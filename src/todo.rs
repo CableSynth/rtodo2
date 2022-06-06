65262062f04ba2e4b5fe0ecbb168a4da7aa16c47
CableSynth <lorencalebbaum@gmail.com>
2022-06-05 20:47:32 -0400

    adding todo lessons

--- src/todo.rs
+++ src/todo.rs
@@ -1,7 +1,14 @@
-
 use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::{env, io};
+use std::io::Read;
+use std::{
+    cell::Cell,
+    fs::{self, DirBuilder},
+    path,
+};
 
-use std::{cell::Cell, collections::LinkedList};
+const TODO_FILE: &str = "~/.rtodo2/todo_file";
 
 #[derive(Debug, Serialize, Deserialize, Copy, Clone)]
 enum Status {
@@ -28,17 +35,22 @@ enum LifeCycle {
     Yearly,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, )]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Todo {
     title: String,
     description: String,
     status: Cell<Status>,
     lifespan: Lifespan,
-    lifecycle: LifeCycle, 
+    lifecycle: LifeCycle,
 }
 
 impl Todo {
-    pub fn new(title: String, description: String, lifespan: Lifespan, lifecycle: LifeCycle) -> Self {
+    pub fn new(
+        title: String,
+        description: String,
+        lifespan: Lifespan,
+        lifecycle: LifeCycle,
+    ) -> Self {
         Self {
             title,
             description,
@@ -46,5 +58,46 @@ impl Todo {
             lifecycle,
             status: Cell::new(Status::Open),
         }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Todos {
+    todos: Vec<Todo>,
+}
+
+impl Todos {
+    fn new() -> Self {
+        Self { todos: vec![] }
+    }
+
+    fn load(&mut self) {
+        let file_path = shellexpand::full(TODO_FILE).unwrap();
+        let path = path::Path::new(file_path.as_ref());
+        let prefix = path.parent().unwrap();
+
+        DirBuilder::new().recursive(true).create(prefix).unwrap();
+        let mut todo_file = fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(file_path.as_ref())
+            .expect("Unable to open file");
+        let mut data = String::new();
+        todo_file
+            .read_to_string(&mut data)
+            .expect("Unable to read file");
+        if data.is_empty() {
+            data = String::from("[]")
+        }
+        self.todos = serde_json::from_str(&data).unwrap();
+    }
+
+    fn get_all(&self) -> &Vec<Todo> {
+        &self.todos
+    }
+
+    fn add(&mut self, todo: Todo) {
+        self.todos.push(todo)
     }
 }
